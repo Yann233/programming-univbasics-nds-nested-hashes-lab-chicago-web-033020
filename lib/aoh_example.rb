eea941a8f343bd5fc829bc85c9a8153be0d61020
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-14 00:44:02 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,12 +11,19 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 def assembled_aoh
 singleArray=[DON_G,JOELLE_VD,PAT_M,KATE_G,BRUCE_G]
 end
-
+####################################################
+# Using Array literal syntax only, build a nested array that uses the data in
+# held in the constants
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+  nested_Array=[
+    { name:  "Don Gately", occupation: "Live-in Staff" },
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    { name:  "Pat Monteseian", occupation: "Staff" },
+    { name:  "Kate Gompert", occupation: "None" },
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  ]
 end
-
+#######################################################
 def aoh_lookup(aoh, row, key)
 end
 
